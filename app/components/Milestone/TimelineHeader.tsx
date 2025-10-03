810e60b77a96db5307e69a16f1f92e8189075b39
fohalabi <ayomideabimbola79@gmail.com>
2025-10-03 18:49:22 +0100

    TimeLine header contents added

---
+++ app/components/Milestone/TimelineHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TimelineHeader: React.FC = () => {
+  return (
+    <div className="relative max-w-4xl mx-auto mb-16">
+      <h2
+        className="
+          m-6 p-4
+          md:mt-6 md:p-0 md:pl-12 md:border-0
+          text-based md:text-2xl font-bold text-gray-900
+          text-center
+          leading-relaxed
+        "
+      >
+        Since inception in 2019, Asido Foundation has led bold advocacy for mental
+        health reforms. These range from public awareness campaigns to the passing
+        of the 2021 Mental Health Act.
+      </h2>
+    </div>
+  );
+};
+
+
+export default TimelineHeader;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection'
+import HeroSection from './components/HeroSection';
+import TimelineHeader from './components/Milestone/TimelineHeader';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <TimelineHeader />
       <h1>Contents goes here!</h1>
     </div>
   );
