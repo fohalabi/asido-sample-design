f7c4d8ad13c5d8e516965998fbc9eacfc7cdb7c7
fohalabi <ayomideabimbola79@gmail.com>
2025-10-03 19:05:36 +0100

    added the progessline

---
+++ app/components/Milestone/Timeline.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProgressLine from './ProgressLine';
+
+const Timeline = () => {
+  return (
+  <div>
+   <ProgressLine scrollProgress={0} />
+  </div>
+  )
+}
+
+export default Timeline
